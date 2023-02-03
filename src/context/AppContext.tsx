93f7ec3e9fa6283201f1a643a34cbadf2d615a10
Adeyemo Adekunle <adekunle@nestcoin.com>
2023-02-03 21:08:24 +0100

    feat : persist pokemons in AsyncStorage

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,4 +1,12 @@
-import React, {useState, createContext, FC, useMemo, useContext} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {
+  useState,
+  createContext,
+  FC,
+  useMemo,
+  useContext,
+  useEffect,
+} from 'react';
 import {IPokemon} from '../types/types';
 
 export const AppStateContext = createContext<IStateContext>(undefined as never);
@@ -16,17 +24,47 @@ type IProp = {
 
 export const AppStateProvider: FC<IProp> = ({children}) => {
   const [pokemons, setPokemons] = useState<IPokemon[]>([]);
+
+  useEffect(() => {
+    retrieveData();
+  }, []);
+
   const favouritePokemon = (pokemon: IPokemon) => {
+    storeData([...pokemons, pokemon]);
     setPokemons([...pokemons, pokemon]);
   };
   const unFavouritePokemon = (pokemon: IPokemon) => {
-    const tempPokemons = pokemons.filter(poke => poke.id != pokemon.id);
+    const tempPokemons: IPokemon[] = pokemons.filter(
+      poke => poke.id != pokemon.id,
+    );
+    storeData(tempPokemons);
     setPokemons(tempPokemons);
   };
   const isFavouritedPokemon = (pokemon: IPokemon): boolean => {
     const poke = pokemons.find(poke => poke.id === pokemon.id);
     return poke ? true : false;
   };
+
+  const storeData = async (pokemons: IPokemon[]) => {
+    try {
+      await AsyncStorage.setItem('pokemons', JSON.stringify(pokemons));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('pokemons');
+      if (value !== null) {
+        const data = JSON.parse(value);
+        setPokemons([...data]);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const value = useMemo(
     () => ({
       pokemons,
