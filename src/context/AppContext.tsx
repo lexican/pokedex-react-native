34233052312219b8b59039d213e968c5b0977542
Adeyemo Adekunle <adekunle@nestcoin.com>
2023-02-03 20:13:16 +0100

    feat : AppStateContext

---
+++ src/context/AppContext.tsx
@@ -0,0 +1,47 @@
+import React, {useState, createContext, FC, useMemo, useContext} from 'react';
+import {IPokemon} from '../types/types';
+
+export const AppStateContext = createContext<IStateContext>(undefined as never);
+
+type IStateContext = {
+  pokemons: IPokemon[];
+  favouritePokemon: (pokemon: IPokemon) => void;
+  unFavouritePokemon: (pokemon: IPokemon) => void;
+  isFavouritedPokemon: (pokemon: IPokemon) => void;
+};
+
+type IProp = {
+  children: JSX.Element;
+};
+
+export const AppStateProvider: FC<IProp> = ({children}) => {
+  const [pokemons, setPokemons] = useState<IPokemon[]>([]);
+  const favouritePokemon = (pokemon: IPokemon) => {
+    setPokemons([...pokemons, pokemon]);
+  };
+  const unFavouritePokemon = (pokemon: IPokemon) => {
+    const tempPokemons = pokemons.filter(poke => poke.id != pokemon.id);
+    setPokemons(tempPokemons);
+  };
+  const isFavouritedPokemon = (pokemon: IPokemon) => {
+    return pokemons.find(poke => poke.id === pokemon.id);
+  };
+  const value = useMemo(
+    () => ({
+      pokemons,
+      favouritePokemon,
+      unFavouritePokemon,
+      isFavouritedPokemon,
+    }),
+    [pokemons],
+  );
+  return (
+    <AppStateContext.Provider value={value}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
+
+export function useAppStateContext(): IStateContext {
+  return useContext(AppStateContext);
+}
